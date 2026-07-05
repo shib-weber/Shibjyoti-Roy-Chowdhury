dcb142c3edfe80f04e25afcb69c740b9cb45dee7
Shib <shibjyoti252@gmail.com>
2026-07-05 10:31:18 +0530

    Servicing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import Portfolio from "./Pages/page";
 import ServicesAndCheckout from "./Pages/ServicesAndCheckout"; // Path to your golden glassmorphism file
 import ScrollToTop from "./components/ScrollToTop";
 import './App.css';
 
 function App() {
+      useEffect(() => {
+  
+  if (window.location.hash) {
+    history.replaceState(null, "", window.location.pathname + window.location.search);
+  }
+
+  // Scroll to top on first load
+  window.scrollTo({ top: 0, behavior: "auto" });
+}, []);
   return (
     <>
       {/* Ensures scroll position drops to 0,0 across every single route transition */}
